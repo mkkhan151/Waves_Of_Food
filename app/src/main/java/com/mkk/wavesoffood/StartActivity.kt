9d22b70d7fc8c5a54849f7b31b3809701d64bdb1
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-03-02 19:34:15 +0500

    create a login activity for app

--- app/src/main/java/com/mkk/wavesoffood/StartActivity.kt
+++ app/src/main/java/com/mkk/wavesoffood/StartActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.mkk.wavesoffood.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-        findViewById<Button>(R.id.nextButton).setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
