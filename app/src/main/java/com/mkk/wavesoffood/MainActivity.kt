133e3d1528dfdcd85af8d7111581c202fda81b1f
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-03-03 16:30:41 +0500

    create navigation for fragments

--- app/src/main/java/com/mkk/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/mkk/wavesoffood/MainActivity.kt
@@ -2,10 +2,17 @@ package com.mkk.wavesoffood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navController = findNavController(R.id.fragmentContainerView)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(navController)
     }
 }
